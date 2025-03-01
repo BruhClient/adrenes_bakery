723bdd5e09f4bde1968268376e9ca26990ae75e8
Travis <Travisang40@gmail.com>
2025-03-01 20:11:53 +0800

    error

--- components/Reviews.tsx
+++ components/Reviews.tsx
@@ -21,7 +21,7 @@ async function Reviews() {
     
 
     return ( <div className="flex flex-col gap-3 px-6 ">
-        <h1 className="text-center text-2xl flex flex-col items-center font-semibold">Reviews <div className="w-12 rounded-lg h-2 bg-secondary"/></h1>
+        <h1 className="text-center text-2xl flex flex-col items-center font-semibold">Reviews<div className="w-12 rounded-lg h-2 bg-secondary"/></h1>
 
         <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-3">
             {data.map((review,index) => <ReviewCard key={index} name={review.name} rating={review.rating} description={review.description} location={review.location} _createdAt={review._createdAt}/>)}
