c53b55987f884b626151e52b3ca1a56e4fea0b48
Travis <Travisang40@gmail.com>
2025-03-01 20:07:33 +0800

    imports

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,7 @@
 import { ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 import { formatDistanceToNowStrict } from 'date-fns'
-import enUS from 'date-fns/locale/en-US'
+import {enUS} from 'date-fns/locale/en-US'
 
 
 
